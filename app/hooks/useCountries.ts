193f7cf1f90054b746e25fc45cbde8e777639ccb
abdulsamet karakayalı <samedkarakayali@gmail.com>
2023-09-30 22:01:40 +0300

    home page design

--- app/hooks/useCountries.ts
+++ app/hooks/useCountries.ts
@@ -4,8 +4,7 @@ import locationsData from '../api/cities/cities_of_turkey.json';
 const formattedLocations = locationsData.map((city) => ({
   value: city.name,
   label: city.name,
-  latlng: [city.latitude,city.longitude],
-  districts: city.region,
+  latlng: [city.latitude,city.longitude]
 }));
 
 const useCountries = () => {

11ee855fa65bae50764041817ec8aa521a79c830
abdulsametkarakayali <samedkarakayali@gmail.com>
2023-11-01 01:52:56 +0300

    some photosh problem solved 0152

--- app/listings/[listingSlug]/[listingId]/ListingClient.tsx
+++ app/listings/[listingSlug]/[listingId]/ListingClient.tsx
@@ -115,7 +115,7 @@ const ListingClient: React.FC<ListingClientProps> = ({
         <div className="flex flex-col gap-6">
           <ListingHead
             title={listing.title}
-            imageSrc={listing.imageSrc}
+            imageSrc={listing.imageSrc as string[]}
             locationValue={listing.locationValue}
             id={listing.id}
             currentUser={currentUser}
